b31e2e694cf4891ccf1fe2e09915e16aaf233120
shwoop <monkey.mambo.al.ferguson@gmail.com>
2026-01-28 22:46:32 +0100

    fix

--- modules/email/outbound.tf
+++ modules/email/outbound.tf
@@ -7,7 +7,7 @@ resource "cloudflare_record" "tem_spf" {
   zone_id = var.zone_id
   name    = var.domain
   type    = "TXT"
-  content = scaleway_tem_domain.this.spf_config
+  content = "v=spf1 include:_spf.mx.cloudflare.net include:_spf.tem.scaleway.com ~all"
   ttl     = 3600
 }
 
@@ -17,13 +17,4 @@ resource "cloudflare_record" "tem_dkim" {
   type    = "TXT"
   content = scaleway_tem_domain.this.dkim_config
   ttl     = 3600
-}
-
-resource "cloudflare_record" "tem_mx" {
-  zone_id  = var.zone_id
-  name     = var.domain
-  type     = "MX"
-  content  = scaleway_tem_domain.this.mx_blackhole
-  priority = 10
-  ttl      = 3600
 }
