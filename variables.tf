f2c8b83847662e9ff9747606fe6ce1f6e49e060f
shwoop <monkey.mambo.al.ferguson@gmail.com>
2026-01-29 21:25:27 +0100

    fmt

--- variables.tf
+++ variables.tf
@@ -12,9 +12,9 @@ variable "domain" {
 variable "dns_records" {
   description = "Map of DNS records to create"
   type = map(object({
-    type    = string
-    name    = string
-    content = string
+    type     = string
+    name     = string
+    content  = string
     ttl      = optional(number, 1) # 1 = automatic
     proxied  = optional(bool, false)
     priority = optional(number, null)
@@ -57,7 +57,7 @@ variable "bucket_name" {
 variable "email_routes" {
   description = "Map of emails to reroute"
   type = map(object({
-    to = string
-    target = string 
+    to     = string
+    target = string
   }))
 }
